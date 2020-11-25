bea64f645b19b18eeb7305cb6dfd238a62ddff37
hazim0001 <hazim_alsayed@hotmail.com>
2020-11-24 23:06:37 -0600

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   devise_scope :employee do
-    root to: 'orders#new'
+    root to: 'pages#home'
   end
 
 
